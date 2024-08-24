745efc76b92f84d24297f099383098abe376cb50
VidValda <valdajosevid@gmail.com>
2024-08-24 18:27:37 -0400

    xd

--- app/main.py
+++ app/main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.api.endpoints import timelapse
 
+import os
+import ee
+import json
+
+# Load credentials from environment variable
+service_account_info = json.loads(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_JSON'))
+credentials = ee.ServiceAccountCredentials('', service_account_info)
+ee.Initialize(credentials)
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
